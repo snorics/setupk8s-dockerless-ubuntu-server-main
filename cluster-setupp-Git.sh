5a991ee94a021a433372ceb56acccd91ca5cde49
snorics <snorics@gmail.com>
2022-06-12 00:30:52 +0200

    Update cluster-setupp-Git.sh

--- cluster-setupp-Git.sh
+++ cluster-setupp-Git.sh
@@ -28,16 +28,16 @@ sudo swapoff -a
 echo "Please remove the swap entry from /etc/fstab !!!"
 
 sudo rm -fr /etc/cni/net.d/*
-sudo apt -y update
+DEBIAN_FRONTEND=noninteractive sudo apt -y update
 
 sudo apt -y install -y apt-transport-https ca-certificates curl
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-sudo apt -y update
-sudo apt -y upgrade
+DEBIAN_FRONTEND=noninteractive sudo apt -y update
+DEBIAN_FRONTEND=noninteractive sudo apt -y upgrade
 
-sudo apt -y install kubelet kubeadm kubectl
+DEBIAN_FRONTEND=noninteractive sudo apt -y install kubelet kubeadm kubectl
 
 
 
@@ -50,8 +50,8 @@ echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers
 echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$CRIO_VERSION/$OS/ /"|sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$CRIO_VERSION.list
 curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$CRIO_VERSION/$OS/Release.key | sudo apt-key add -
 curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | sudo apt-key add -
-sudo apt -y update
-sudo apt -y install cri-o cri-o-runc
+DEBIAN_FRONTEND=noninteractive sudo apt -y update
+DEBIAN_FRONTEND=noninteractive sudo apt -y install cri-o cri-o-runc
 sudo systemctl enable crio.service
 sudo systemctl start crio.service
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16
@@ -65,6 +65,8 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl get pods -A
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/k8s-manifests/kube-flannel-rbac.yml
+sleep 3
 kubectl get pods -A
+
 
 
