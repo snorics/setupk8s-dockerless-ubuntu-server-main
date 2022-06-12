249abfa73d7e41ead74ed9b75c8e0f1a938d70fc
snorics <snorics@gmail.com>
2022-06-12 03:58:52 +0200

    Update cluster-setupp-Git.sh

--- cluster-setupp-Git.sh
+++ cluster-setupp-Git.sh
@@ -60,6 +60,7 @@ sudo apt-get install apt-transport-https --yes
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 sudo apt-get update
 sudo apt-get install helm
+helm repo add bitnami https://charts.bitnami.com/bitnami
 
 
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
